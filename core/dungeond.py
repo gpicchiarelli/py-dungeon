3e4afe27bb5ab3e0ac0516454f91ca6afbec9ff0
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2019-08-25 13:55:46 +0200

    daemon standard process

--- core/dungeond.py
+++ core/dungeond.py
@@ -26,7 +26,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 import sys, time
-from daemon import Daemon
+import daemon
 
 class DungeonDaemon(Daemon):
         def run(self):
